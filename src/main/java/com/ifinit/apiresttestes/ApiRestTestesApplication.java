78c71b360eba82bbefa1ba8220a493e45d906af9
saulocapistrano <saulocapistrano@gmail.com>
2023-11-10 20:33:25 -0300

    projeto iniciado

---
+++ src/main/java/com/ifinit/apiresttestes/ApiRestTestesApplication.java
@@ -0,0 +1,13 @@
+package com.ifinit.apiresttestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestTestesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestTestesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ifinit/apiresttestes/ApiRestTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifinit.apiresttestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestTestesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
