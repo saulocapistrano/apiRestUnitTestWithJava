df56655e9c3e079c9f54b17d9192b4d773cad33d
saulocapistrano <saulocapistrano@gmail.com>
2023-11-11 05:26:49 -0300

    create interfaces exception

---
+++ src/main/java/com/ifinit/apiresttestes/services/exceptions/DataIntegratyViolationException.java
@@ -0,0 +1,9 @@
+package com.ifinit.apiresttestes.services.exceptions;
+
+public class DataIntegratyViolationException extends RuntimeException {
+
+    public DataIntegratyViolationException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/ifinit/apiresttestes/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ifinit.apiresttestes.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+}
