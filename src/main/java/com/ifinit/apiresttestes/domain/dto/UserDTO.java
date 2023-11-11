1b38fc538ea72133938c8a1ee098d605a5d330d3
saulocapistrano <saulocapistrano@gmail.com>
2023-11-11 05:24:44 -0300

    create class userdto

---
+++ src/main/java/com/ifinit/apiresttestes/domain/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.ifinit.apiresttestes.domain.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+
+
+    private Integer id;
+    private String name;
+    private String mail;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
