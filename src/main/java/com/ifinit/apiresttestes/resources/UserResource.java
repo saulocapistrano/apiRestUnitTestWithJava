4d8eac581dbf172ba9131638ddd319243317cb5a
saulocapistrano <saulocapistrano@gmail.com>
2023-11-12 11:00:29 -0300

    refactor to user resoucer

--- src/main/java/com/ifinit/apiresttestes/resources/UserResource.java
+++ src/main/java/com/ifinit/apiresttestes/resources/UserResource.java
@@ -5,6 +5,7 @@ import com.ifinit.apiresttestes.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user")
+@Service
 public class UserResource {
 
     private static final String ID = "/{id}";
