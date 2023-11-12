e0c8467dd29da1ab3bce242c38f3d8eb91122901
saulocapistrano <saulocapistrano@gmail.com>
2023-11-12 15:32:09 -0300

    create test to manipuled to integrity violation method, object not found

--- src/test/java/com/ifinit/apiresttestes/resources/exception/ResourceExceptionHandlerTest.java
+++ src/test/java/com/ifinit/apiresttestes/resources/exception/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package com.ifinit.apiresttestes.resources.exception;
 
+import com.ifinit.apiresttestes.services.exceptions.DataIntegratyViolationException;
 import com.ifinit.apiresttestes.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,11 +12,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 
+    public static final String THIS_EMAIL_IS_ALREADY_REGISTERED_IN_THE_SYSTEM = "This email is already registered in the system.";
     @InjectMocks
     private  ResourceExceptionHandler exceptionHandler;
 
@@ -39,10 +43,24 @@ class ResourceExceptionHandlerTest {
         Assertions.assertEquals(StandardError.class, response.getBody().getClass());
         Assertions.assertEquals("Object not found", response.getBody().getError());
         Assertions.assertEquals(404, response.getBody().getStatus());
+        Assertions.assertNotEquals("/user/2", response.getBody().getPath());
+        Assertions.assertNotEquals(LocalDateTime.now(), response.getBody().getTimestemp());
 
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void whenDataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegrityViolationException(
+                        new DataIntegratyViolationException(THIS_EMAIL_IS_ALREADY_REGISTERED_IN_THE_SYSTEM),
+                        new MockHttpServletRequest());
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assertions.assertEquals(ResponseEntity.class, response.getClass());
+        Assertions.assertEquals(StandardError.class, response.getBody().getClass());
+        Assertions.assertEquals(THIS_EMAIL_IS_ALREADY_REGISTERED_IN_THE_SYSTEM, response.getBody().getError());
+        Assertions.assertEquals(400, response.getBody().getStatus());
+
     }
 }
